e243a4562e66f42763d8f5f4c79f23778e1a578e
Everett Toews <everett.toews@gmail.com>
2024-12-15 20:47:52 +1300

    Fix formatting

--- services/tasks_api/tests.py
+++ services/tasks_api/tests.py
@@ -19,4 +19,3 @@ def test_health_check(client):
     response = client.get("/api/health-check/")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"message": "OK"}
-
