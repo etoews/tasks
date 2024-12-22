a4b2ff11d69eab9517d5714d51c3bb64e2b7f903
Everett Toews <everett.toews@gmail.com>
2024-12-23 11:02:18 +1300

    Task API CRUD

--- services/tasks_api/main.py
+++ services/tasks_api/main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+import uuid  # new
+from typing import Union  # new
+
+import jwt  # new
+from fastapi import Depends, FastAPI, Header  # new
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
+from starlette import status  # new
+
+from config import Config  # new
+from models import Task  # new
+from schemas import APITask, APITaskList, CloseTask, CreateTask  # new
+from store import TaskStore  # new
 
 app = FastAPI()
 
@@ -12,10 +22,63 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+config = Config()
+handle = Mangum(app)
+
+
+def get_task_store() -> TaskStore:
+    return TaskStore(config.TABLE_NAME, dynamodb_url=config.DYNAMODB_URL)
+
+
+def get_user_email(authorization: Union[str, None] = Header(default=None)) -> str:
+    return jwt.decode(authorization, options={"verify_signature": False})[
+        "cognito:username"
+    ]
+
 
 @app.get("/api/health-check/")
 def health_check():
     return {"message": "OK"}
 
 
-handle = Mangum(app)
+@app.post(
+    "/api/create-task", response_model=APITask, status_code=status.HTTP_201_CREATED
+)
+def create_task(
+    parameters: CreateTask,
+    user_email: str = Depends(get_user_email),
+    task_store: TaskStore = Depends(get_task_store),
+):
+    task = Task.create(id_=uuid.uuid4(), title=parameters.title, owner=user_email)
+    task_store.add(task)
+
+    return task
+
+
+@app.get("/api/open-tasks", response_model=APITaskList)
+def open_tasks(
+    user_email: str = Depends(get_user_email),
+    task_store: TaskStore = Depends(get_task_store),
+):
+    return APITaskList(results=task_store.list_open(owner=user_email))
+
+
+@app.post("/api/close-task", response_model=APITask)
+def close_task(
+    parameters: CloseTask,
+    user_email: str = Depends(get_user_email),
+    task_store: TaskStore = Depends(get_task_store),
+):
+    task = task_store.get_by_id(task_id=parameters.id, owner=user_email)
+    task.close()
+    task_store.add(task)
+
+    return task
+
+
+@app.get("/api/closed-tasks", response_model=APITaskList)
+def closed_tasks(
+    user_email: str = Depends(get_user_email),
+    task_store: TaskStore = Depends(get_task_store),
+):
+    return APITaskList(results=task_store.list_closed(owner=user_email))
